ae14d74cf71b67bc113878e1fb5cc3f7d28212ad
Zhang Tianzhu <zhuzhuaiqiqi1314@gmail.com>
2019-03-09 11:29:32 +0100

    latency test

--- bess-local/p2v.sh
+++ bess-local/p2v.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 export VM_NAME=vhost-vm
-export GUEST_MEM=2048M
+export GUEST_MEM=4096M
 export QCOW2_IMAGE=/home/tianzhu/centos7.qcow2
 export CDROM=/home/tianzhu/CentOS-7-x86_64-Azure.qcow2
 export VHOST_SOCK_DIR=/tmp/bess
 
 cd /home/tianzhu/qemu/bin/x86_64-softmmu/
-sudo numactl --membind=0 --physcpubind=4-7 ./qemu-system-x86_64 -name $VM_NAME -cpu host -enable-kvm \
+sudo numactl --membind=0 --physcpubind=4-7 /usr/bin/qemu-system-x86_64 -name $VM_NAME -cpu host -enable-kvm \
   -m $GUEST_MEM -drive file=$CDROM --nographic \
   -numa node,memdev=mem -mem-prealloc -smp sockets=1,cores=4 \
   -object memory-backend-file,id=mem,size=$GUEST_MEM,mem-path=/dev/hugepages,share=on \
