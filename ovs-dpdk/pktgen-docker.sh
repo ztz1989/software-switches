96c1b84d64882576c62481f3eac9ac06d2557300
Zhang Tianzhu <zhuzhuaiqiqi1314@gmail.com>
2019-07-14 11:39:34 +0200

    bess bidirectional test for 4/5 VNFs

--- ovs-dpdk/pktgen-docker.sh
+++ ovs-dpdk/pktgen-docker.sh
@@ -2,4 +2,4 @@
 
 export VHOST_SOCK_DIR=/usr/local/var/run/openvswitch
 
-sudo docker run -it --name=ovs-pktgen-docker -v /dev/hugepages:/dev/hugepages -v ${VHOST_SOCK_DIR}:/var/run/openvswitch --privileged pktgen-3.1.1
+sudo docker run -it --name=ovs-pktgen-docker-1 -v /dev/hugepages:/dev/hugepages -v ${VHOST_SOCK_DIR}:/var/run/openvswitch --privileged pktgen-3.1.1
--- vpp/pktgen-docker.sh
+++ vpp/pktgen-docker.sh
@@ -2,4 +2,4 @@
 
 export VHOST_SOCK_DIR=/tmp/vpp
 
-sudo docker run -it --name=pktgen-vpp-docker -v /dev/hugepages:/dev/hugepages -v ${VHOST_SOCK_DIR}:/tmp/vpp --privileged pktgen-3.1.1
+sudo docker run -it --name=pktgen-vpp-docker-1 -v /dev/hugepages:/dev/hugepages -v ${VHOST_SOCK_DIR}:/tmp/vpp --privileged pktgen-3.1.1
