7e51c0cdc6234bd19b42ea3974ed1e875fb7f815
Zhang Tianzhu <zhuzhuaiqiqi1314@gmail.com>
2019-09-20 11:54:25 +0200

    Delete pktgen-docker.sh

--- ovs-dpdk/pktgen-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export VHOST_SOCK_DIR=/usr/local/var/run/openvswitch
-
-sudo docker run -it --name=ovs-pktgen-docker-1 -v /dev/hugepages:/dev/hugepages -v ${VHOST_SOCK_DIR}:/var/run/openvswitch --privileged pktgen-3.1.1
