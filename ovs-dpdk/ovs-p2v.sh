56e2afbe470de07157629dcd29737616f8689a41
Zhang Tianzhu <zhuzhuaiqiqi1314@gmail.com>
2019-09-20 12:00:58 +0200

    Update ovs-p2v.sh

--- ovs-dpdk/ovs-p2v.sh
+++ ovs-dpdk/ovs-p2v.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # A script to start OVS-DPDK switch. The flow table is populated with simple L2 switching rules.
+PCI0="0b:00.0"
 
 # Stop running instances
 sudo env "PATH=${PATH}" ovs-ctl stop
@@ -12,11 +13,11 @@ export DB_SOCK=/usr/local/var/run/openvswitch/db.sock
 sudo ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-init=true
 sudo ovs-vsctl --no-wait set Open_vSwitch . other_config:pmd-cpu-mask=200
 sudo ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-socket-mem="2048,0"
-sudo env "PATH=${PATH}" ovs-ctl --no-ovsdb-server --db-sock="$DB_SOCK" start
+sudo env "PATH=${PATH}" ovs-ctl --no-ovsdb-server --db-sock="${DB_SOCK}" start
 
 sudo ovs-vsctl del-br br-acl
 sudo ovs-vsctl add-br br-acl -- set bridge br-acl datapath_type=netdev
-sudo ovs-vsctl add-port br-acl dpdk-lc0p0 -- set interface dpdk-lc0p0 type=dpdk options:dpdk-devargs=0000:0b:00.0 ofport_request=1
+sudo ovs-vsctl add-port br-acl dpdk-lc0p0 -- set interface dpdk-lc0p0 type=dpdk options:dpdk-devargs="${PCI0}" ofport_request=1
 
 sudo ovs-vsctl add-port br-acl vhost-user-0 -- set Interface vhost-user-0 type=dpdkvhostuser options:n_rxq=1 ofport_request=3
 
